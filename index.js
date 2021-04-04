b04984d89c021afa2556b9830057dd7c6b5b97a8
Yo Yehudi <yochannah@gmail.com>
2021-04-04 16:43:53 +0100

    migrat to oktokit since it is built into the api examples

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-var github = require('octonode');
-var client = github.client(process.env.github_sustain_sw_token);
+const { Octokit } = require("@octokit/core");
 
-var ghrepo = client.repo('open-life-science/open-life-science.github.io');
-
-ghrepo.info(function(_, response, server){
-  console.log(_, response);
+const octokit = new Octokit({
+  auth: process.env.github_sustain_sw_token,
 });
+
+var ghrepo = 'open-life-science/open-life-science.github.io';
+
+
+// Start function
+const start = async function() {
+  const result = await octokit.request('GET /repos/{owner}/{repo}/community/code_of_conduct', {
+      "owner": 'open-life-science',
+      "repo": 'open-life-science.github.io',
+      "mediaType": {'previews': ['scarlet-witch']}})
+  console.log(result);
+}
+
+// Call start
+start();
