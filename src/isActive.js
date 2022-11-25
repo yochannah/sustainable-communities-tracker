93cdd99f51c6561a25f7f76bf86eef3bb556dc3c
Yo Yehudi <yochannah@gmail.com>
2022-11-25 16:55:51 +0000

    delete redundant code

--- src/isActive.js
+++ src/isActive.js
@@ -1,32 +1,22 @@
-const {
-    Octokit
-  } = require("@octokit/core");
-  const {
-    throttling
-  } = require("@octokit/plugin-throttling");
-  const parse_headers = require('parse-link-header');
-  const fs = require('fs');
-  const errorHandler = require('./errorHandler.js');
-  const { DateTime } = require("luxon");
-  const path = require('path'),
-    errors = require('./errors.js'),
-    fm = require('./fileManager.js'),
-    messages = require('./messages.js');
-  const { initOcto, checkNoOfResults, countPaginatedResults, splitUrl } = require("./app.js");
-  
-  var octokit;
 
-  async function isActive(config, anOctokit) {
-    config.owner = config.org;
-    config.repo = config.repo.trim();
-    octokit = anOctokit || initOcto();
-    let results = checkNoOfResults(config, "commits").then(function(response){
-      countPaginatedResults(config, response, "commits").then(function(count){
-      console.log('%c ISACTIVE: 🤓 ',count);
-   });
-   
+const { DateTime } = require("luxon");
+const { initOcto, checkNoOfResults, countPaginatedResults, splitUrl } = require("./app.js");
+
+function isActive(config, anOctokit) {
+  config.owner = config.org;
+  config.repo = config.repo.trim();
+  const octokit = anOctokit || initOcto();
+  return results = new Promise(function (resolve, reject) {
+    checkNoOfResults(config, "commits").then(function (response) {
+      countPaginatedResults(config, response, "commits").then(function (count) {
+        resolve({
+          commitCount: count,
+          config: config,
+          dateRetrieved: DateTime.now().toString()
+        });
+      });
+    });
   });
-
 }
 
-module.exports = {isActive};
+module.exports = { isActive };
