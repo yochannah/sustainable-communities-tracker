d72c462e5dfcfa68567e126e1c6477108d1ffa90
Yo Yehudi <yochannah@gmail.com>
2023-06-08 18:04:16 +0100

    add status page, add favicon, scaffold for expectations 3-data-point graph

--- view/_plugins/repo_page_generator.rb
+++ view/_plugins/repo_page_generator.rb
@@ -6,28 +6,30 @@ module PageGeneratorPlugin
 
     def generate(site)
       site.data['repo']['repo'].each do |repo|
-        site.pages << RepoPage.new(site, repo['reponame'])
+        site.pages << RepoPage.new(site, repo['reponame'], "repo")
+        site.pages << RepoPage.new(site, repo['reponame'], "survey")
       end
     end
   end
 
   # Subclass of `Jekyll::Page` with custom method definitions.
   class RepoPage < Jekyll::Page
-    def initialize(site, reponame)
+    def initialize(site, reponame, pageType)
       @site = site             # the current site instance.
       @base = site.source      # path to the source directory.
-      @dir  = 'repos'         # the directory the page will reside in.
+      @dir  = pageType         # the directory the page will reside in.
 
       # All pages have the same filename, so define attributes straight away.
       @basename = reponame      # filename without the extension.
       @ext      = '.html'      # the extension.
+      @pagetype = pageType
       @name     = @basename + @ext # basically @basename + @ext.
 
       # Initialize data hash with a key pointing to all posts under current category.
       # This allows accessing the list in a template via `page.linked_docs`.
       @data = {
         'reponame' => reponame,
-        "layout" => "single_repo"
+        "layout" => pageType
       }
 
     end
@@ -36,7 +38,7 @@ module PageGeneratorPlugin
     # Placeholders that are used in constructing page URL.
     def url_placeholders
       {
-        :path   => "repo",
+        :path   => @pagetype,
         :basename   => basename,
         :output_ext => output_ext
       }
