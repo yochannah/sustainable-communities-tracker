4588bc958b499b43f19291ae2d0d8d6c653d8ab8
Yo Yehudi <yochannah@gmail.com>
2023-06-10 16:59:21 +0100

    add labels for datapoints, should mean graphs are colourblind friendly too

--- view/scripts.js
+++ view/scripts.js
@@ -1,17 +1,16 @@
 const generateElem = function (variable, anAnchor, legend) {
     let chartElemHtml = `<canvas id="chart${variable}" width="400" height="400" class="aggGraph"></canvas>`;
     let a;
-    if (anAnchor)
-    {a = anAnchor;} else {a = "aggregateAnchor"}
+    if (anAnchor) { a = anAnchor; } else { a = "aggregateAnchor" }
     let anchor = document.getElementById(a);
 
     if (legend) {
         legendHtml = `<div id="legend${variable}" class="legend"></div>`;
-        chartElemHtml = legendHtml+chartElemHtml;
+        chartElemHtml = legendHtml + chartElemHtml;
     }
 
     anchor.innerHTML += chartElemHtml;
-}   
+}
 
 const getOrCreateLegendList = (chart, id) => {
     const legendContainer = document.getElementById(id);
@@ -58,7 +57,7 @@ const htmlLegendPlugin = {
 
             // Text
             const textContainer = document.createElement('p');
-   
+
             const text = document.createTextNode(item);
             textContainer.appendChild(text);
 
@@ -69,185 +68,229 @@ const htmlLegendPlugin = {
     }
 };
 
-function prepData(variable) {
-    let responses = {}
-    survey0.map(function(row) {
-      let proj = row.ProjectPseudonym.trim();
-      responses[proj] = {
-        m0: row[variable]
-      };
+function prepData(variable, sortBy) {
+    let responses = {};
+
+    if (sortBy) {
+        survey0.sort(function (a, b) {
+            if (sortBy == "age") {
+                let ages = agesByProjName;
+                let aDate = new Date(ages[a.ProjectPseudonym]);
+                let bDate = new Date(ages[b.ProjectPseudonym]);
+                if (aDate < bDate) {
+                    return -1;
+                }
+                if (bDate < aDate) {
+                    return 1;
+                }
+            }
+            else {
+                if (a[sortBy] < b[sortBy]) {
+                    return -1;
+                }
+                if (b[sortBy] < a[sortBy]) {
+                    return 1;
+                }
+            }
+            //if all else failed, they're equal: 
+            return 0;
+        });
+    }
+
+    survey0.map(function (row) {
+        let proj = row.ProjectPseudonym.trim();
+        responses[proj] = {
+            m0: row[variable]
+        };
     });
 
-    survey6.map(function(row) {
-      let proj = row.ProjectPseudonym.trim();
-      responses[proj].m6 = row[variable];
+    survey6.map(function (row) {
+        let proj = row.ProjectPseudonym.trim();
+        responses[proj].m6 = row[variable];
 
     });
-    survey12.map(function(row) {
-      let proj = row.ProjectPseudonym.trim();
-      responses[proj].m12 = row[variable];
+    survey12.map(function (row) {
+        let proj = row.ProjectPseudonym.trim();
+        responses[proj].m12 = row[variable];
     });
 
-// stripe the data, so the chart likes it.
+    // stripe the data, so the chart likes it.
     let visData = {
-      names: Object.keys(responses),
-      m0: [],
-      m6: [],
-      m12: [],
-      colors: {
-        bg: {
-          m0: [],
-          m6: [],
-          m12: []
-        },
-        border: {
-          m0: [],
-          m6: [],
-          m12: []
+        names: Object.keys(responses),
+        m0: [],
+        m6: [],
+        m12: [],
+        colors: {
+            bg: {
+                m0: [],
+                m6: [],
+                m12: []
+            },
+            border: {
+                m0: [],
+                m6: [],
+                m12: []
+            }
         }
-      }
     };
 
-    Object.values(responses).map(function(e) {
-      visData.m0.push(e.m0);
-      visData.m6.push(e.m6);
-      visData.m12.push(e.m12);
-      visData
-        .colors
-        .bg
-        .m0
-        .push(colorForBool(e.m0, true));
-      visData
-        .colors
-        .bg
-        .m6
-        .push(colorForBool(e.m6, true));
-      visData
-        .colors
-        .bg
-        .m12
-        .push(colorForBool(e.m12, true));
-      visData
-        .colors
-        .border
-        .m0
-        .push(colorForBool(e.m0));
-      visData
-        .colors
-        .border
-        .m6
-        .push(colorForBool(e.m6));
-      visData
-        .colors
-        .border
-        .m12
-        .push(colorForBool(e.m12));
+    Object.values(responses).map(function (e) {
+        visData.m0.push(e.m0);
+        visData.m6.push(e.m6);
+        visData.m12.push(e.m12);
+        visData
+            .colors
+            .bg
+            .m0
+            .push(colorForBool(e.m0, true));
+        visData
+            .colors
+            .bg
+            .m6
+            .push(colorForBool(e.m6, true));
+        visData
+            .colors
+            .bg
+            .m12
+            .push(colorForBool(e.m12, true));
+        visData
+            .colors
+            .border
+            .m0
+            .push(colorForBool(e.m0));
+        visData
+            .colors
+            .border
+            .m6
+            .push(colorForBool(e.m6));
+        visData
+            .colors
+            .border
+            .m12
+            .push(colorForBool(e.m12));
     });
     return visData;
-  }
+}
 
-  var colForBool = {
+var colForBool = {
     "Yes": {
-      border: colors.solid.blue,
-      bg: colors.faded.blue
+        border: colors.solid.blue,
+        bg: colors.faded.blue
     },
     "No": {
-      border: colors.solid.yellow,
-      bg: colors.faded.yellow
+        border: colors.solid.yellow,
+        bg: colors.faded.yellow
     },
     "No answer": {
-      border: colors.solid.grey,
-      bg: colors.faded.grey
+        border: colors.solid.grey,
+        bg: colors.faded.grey
     }
-  }
+}
 
 // I know it's not a real bool, but functionally it's a bool!
-  const colorForBool = function(bool, bg) {
-    if (! bool) {
-      return colors.faded.grey; // grey for null values
-    }if (bg) {
-      return colForBool[bool].bg;
+const colorForBool = function (bool, bg) {
+    if (!bool) {
+        return colors.faded.grey; // grey for null values
+    } if (bg) {
+        return colForBool[bool].bg;
     } else {
-      return colForBool[bool].border;
+        return colForBool[bool].border;
     }
 
-  }
+}
 
-  const generateExpChart = function(variable) {
+const generateExpChart = function (variable, sortBy) {
     try {
-      let visData = prepData(variable);
-      let fill = Array(visData.m0.length).fill(1);
+        let visData = prepData(variable, sortBy);
+        let fill = Array(visData.m0.length).fill(1);
 
-// this positions the labels on half-ticks, which looks a lot better.
-      let months = [
-        null,
-        "Month 0",
-        null,
-        "Month 6",
-        null,
-        "Month 12"
-      ];
+        // this positions the labels on half-ticks, which looks a lot better.
+        let months = [
+            null,
+            "Month 0",
+            null,
+            "Month 6",
+            null,
+            "Month 12"
+        ];
 
-      return new Chart(document.getElementById('chart' + variable), {
-        type: 'bar',
-        data: {
-          labels: visData.names,
-          datasets: [
-            {
-
-              data: fill,
-              backgroundColor: visData.colors.bg.m0,
-              borderColor: visData.colors.border.m0,
-              stack: 'Stack 0'
-            }, {
-              data: fill,
-              backgroundColor: visData.colors.bg.m6,
-              borderColor: visData.colors.border.m6,
-              stack: 'Stack 0'
-            }, {
-              data: fill,
-              backgroundColor: visData.colors.bg.m12,
-              borderColor: visData.colors.border.m12,
-              stack: 'Stack 0'
-            }
-          ]
-        },
-        options: {
-          scales: {
-            x: {
-              ticks: {
-                callback: function(value, index, ticks) {
-                  return months[index];
+        function datalabelsFormatter(dataset) {
+            return function (value, context) {
+                if (dataset[context.dataIndex]) {
+                    return dataset[context.dataIndex]
                 }
-              }
+                return "";
+            }
+        }
+
+        return new Chart(document.getElementById('chart' + variable), {
+            type: 'bar',
+            data: {
+                labels: visData.names,
+                datasets: [
+                    {
+
+                        data: fill,
+                        backgroundColor: visData.colors.bg.m0,
+                        borderColor: visData.colors.border.m0,
+                        stack: 'Stack 0',                        datalabels: {
+                            formatter: datalabelsFormatter(visData.m0)
+                        }
+                    }, {
+                        data: fill,
+                        backgroundColor: visData.colors.bg.m6,
+                        borderColor: visData.colors.border.m6,
+                        stack: 'Stack 0',
+                        datalabels: { 
+                            formatter: datalabelsFormatter(visData.m6) 
+                        }
+                    }, {
+                        data: fill,
+                        datalabels: {
+                            formatter: datalabelsFormatter(visData.m12)
+                        },
+                        backgroundColor: visData.colors.bg.m12,
+                        borderColor: visData.colors.border.m12,
+                        stack: 'Stack 0'
+                    }
+                ]
             },
-            y: {
-              ticks: {
-                callback: function(value, index, ticks) {
-                  return this.getLabelForValue(value);
+            options: {
+                scales: {
+                    x: {
+                        ticks: {
+                            callback: function (value, index, ticks) {
+                                return months[index];
+                            }
+                        }
+                    },
+                    y: {
+                        ticks: {
+                            callback: function (value, index, ticks) {
+                                return this.getLabelForValue(value);
+                            },
+                            autoSkip: false
+                        }
+                    }
                 },
-                autoSkip: false
-              }
-            }
-          },
-          indexAxis: 'y',
-          plugins: {
-            title: {
-              text: questionText[variable],
-              display: true
+                indexAxis: 'y',
+                plugins: {
+                    title: {
+                        text: questionText[variable],
+                        display: true
+                    },
+                    htmlLegend: {
+                        containerID: `legend${variable}`,
+                        boxes: colForBool
+                    },
+                    legend: {
+                        display: false
+                    }
+                }
             },
-            htmlLegend: {
-              containerID: `legend${variable}`,
-              boxes: colForBool
-            },
-            legend: {
-              display: false
-            }
-          }
-        },
-        plugins: [htmlLegendPlugin]
-      });
+            plugins: [htmlLegendPlugin]
+        });
     } catch (e) {
-      console.log(e);
+        console.log(e);
     }
-  }
+}
