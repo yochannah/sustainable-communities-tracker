299be7ac20c42b29632c8299bb74bcfee2412b2f
Yochannah Yehudi <yo@00369-Mac.local>
2023-09-05 14:30:48 +0100

    launch site even if not all data files are local

--- view/_plugins/repo_page_generator.rb
+++ view/_plugins/repo_page_generator.rb
@@ -5,9 +5,11 @@ module PageGeneratorPlugin
     safe true
 
     def generate(site)
-      site.data['repo']['repo'].each do |repo|
-        site.pages << RepoPage.new(site, repo['reponame'], "repo")
-        site.pages << RepoPage.new(site, repo['reponame'], "survey")
+      if site.data["repo"]
+        site.data["repo"]["repo"].each do |repo|
+          site.pages << RepoPage.new(site, repo["reponame"], "repo")
+          site.pages << RepoPage.new(site, repo["reponame"], "survey")
+        end
       end
     end
   end
@@ -17,30 +19,28 @@ module PageGeneratorPlugin
     def initialize(site, reponame, pageType)
       @site = site             # the current site instance.
       @base = site.source      # path to the source directory.
-      @dir  = pageType         # the directory the page will reside in.
+      @dir = pageType         # the directory the page will reside in.
 
       # All pages have the same filename, so define attributes straight away.
       @basename = reponame      # filename without the extension.
-      @ext      = '.html'      # the extension.
+      @ext = ".html"      # the extension.
       @pagetype = pageType
-      @name     = @basename + @ext # basically @basename + @ext.
+      @name = @basename + @ext # basically @basename + @ext.
 
       # Initialize data hash with a key pointing to all posts under current category.
       # This allows accessing the list in a template via `page.linked_docs`.
       @data = {
-        'reponame' => reponame,
-        "layout" => pageType
+        "reponame" => reponame,
+        "layout" => pageType,
       }
-
     end
-
 
     # Placeholders that are used in constructing page URL.
     def url_placeholders
       {
-        :path   => @pagetype,
-        :basename   => basename,
-        :output_ext => output_ext
+        :path => @pagetype,
+        :basename => basename,
+        :output_ext => output_ext,
       }
     end
   end
